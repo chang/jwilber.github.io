aa532e0607d0e5f8839e06b921d9b91fed7a27ba
Jared Wilber <j.wilber92@gmail.com>
2017-11-29 22:27:55 -0800

    new d3 for index

---
+++ index.js
@@ -0,0 +1,123 @@
+//Width and height
+		var w = 900;
+		var h = 900;
+		var damper = 0.102;
+
+		function charge(d) {
+		  // return -Math.pow(d.radius, 2.0) / 2;
+		  return d.radius * 10;
+		};
+
+		function moveToCenter(alpha) {
+			  return function (d) {
+			    d.x = d.x + (center.x - d.x) * damper * alpha;
+			    d.y = d.y + (center.y - d.y) * damper * alpha;
+			  };
+			};
+
+		var colors = d3.scale.quantize()
+					.domain([0,3])
+					.range(['#7f3b08', '#b35806', '#e08214', '#fdb863', '#fee0b6', '#f7f7f7', '#d8daeb', '#b2abd2', '#8073ac']);
+
+		var svg = d3.select('#chart')
+				.append('svg')
+				.attr('width', w)
+				.attr('height', h)
+				.append('g');
+		
+		//Original data
+		var dataset = {
+			nodes: [
+				{ name: "ABOUT ME", radius:70, loc: 'https://jwilber.github.io/about.html'},
+				{ name: "PROJECTS", radius:80,loc: 'https://jwilber.github.io/projects.html' },
+				{ name: "CONTACT", radius:70,loc: 'https://jwilber.github.io/contact.html' },
+				// { name: "Project 4", radius: 10}
+
+			],
+			edges: [
+				{ source: 1, target: 0},
+				{ source: 0, target: 2 },
+				{source: 2, target: 1},
+			]
+		};
+
+
+		var center = {x: w/2, y: h/2.5};
+
+		var force = d3.layout.force()
+						.nodes(dataset.nodes)
+						.links(dataset.edges)
+						.size([w, h])
+						.linkDistance([500])
+						.charge(charge)
+						.friction(.9)
+						.gravity(-.001)
+						.start();
+
+		var edges = svg.selectAll('line')
+					.data(dataset.edges)
+					.enter()
+					.append('line')
+					.style("stroke", "#ccc")
+					.style("stroke-width", 1);
+
+		var nodes = svg.selectAll('circle')
+					.data(dataset.nodes)
+					.enter()
+					.append('circle')
+					.classed('node', true)
+					.attr('r', function(d) {
+						return d.radius;
+					})
+					.style('fill', '#636363')
+					.attr('loc', function(d) {
+						return d.loc;
+					})
+					.call(force.drag);
+
+		var label = svg.selectAll(".mytext")
+                    .data(dataset.nodes)
+                    .enter()
+                    .append("text")
+                    .text(function(d) {
+                    	return d.name;
+                    })
+                    .style("text-anchor", "middle")
+                    .style("fill", "#f0f0f0")
+                    .style("font-family", "futura")
+                    .style('font-weight', 'bold')
+                    .style("font-size", 20);
+
+
+
+		force.on('tick', function (e) {
+
+			edges.attr("x1", function(d) { return d.source.x; })
+		 	 .attr("y1", function(d) { return d.source.y; })
+		 	 .attr("x2", function(d) { return d.target.x; })
+		 	 .attr("y2", function(d) { return d.target.y; });
+
+		    nodes.each(moveToCenter(e.alpha))
+		      .attr('cx', function (d) { return d.x; })
+		      .attr('cy', function (d) { return d.y; });
+
+		    label.attr("x", function(d){ return d.x - 3; })
+             .attr("y", function (d) {return d.y; });
+		  });
+
+
+		d3.selectAll('.node')
+			.on('mouseover', function() {
+				r = parseInt(d3.select(this).attr('r')) + 10;
+				d3.select(this)
+					.attr('r', r);
+			})
+			.on('mouseout', function() {
+				r = parseInt(d3.select(this).attr('r')) - 10;
+				d3.select(this)
+					.attr('r', r);
+			})
+			.on('click', function () {
+					site = d3.select(this).attr('loc');
+                    location.href = site;
+                 	});
